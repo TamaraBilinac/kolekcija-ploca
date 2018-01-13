afa2d362dcff8d1364a5906e7ea801b11e74182c
Nemanja Bilinac <nemanja.bilinac@powerhouse.nl>
2018-01-13 18:53:51 +0100

    Fixnut resolver bug

--- src/main/java/rs/tfzr/kolekcijaploca/config/WebMvcConfig.java
+++ src/main/java/rs/tfzr/kolekcijaploca/config/WebMvcConfig.java
@@ -1,36 +1,30 @@
 package rs.tfzr.kolekcijaploca.config;
 
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @EnableTransactionManagement
 
-public class WebMvcConfig extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer config) {
+		config.enable();
+	}
 
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer config) {
-        config.enable();
-    }
-
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("resources/templates/");
-        resolver.setSuffix("html");
-        return resolver;
-    }
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("templates/");
+		resolver.setSuffix(".html");
+		return resolver;
+	}
 
 }
